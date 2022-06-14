c83d0052c1c630572b511ef6fef7c4ba131a8006
Sureka-22 <101448650+Sureka-22@users.noreply.github.com>
2022-06-15 05:27:58 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Presidio Campus Exam</title>
+<link  rel="stylesheet" href="css/bootstrap.min.css"/>
+ <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
+ <link rel="stylesheet" href="css/main.css">
+ <link  rel="stylesheet" href="css/font.css">
+ <script src="js/jquery.js" type="text/javascript"></script>
+
+  <script src="js/bootstrap.min.js"  type="text/javascript"></script>
+ 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+<?php if(@$_GET['w'])
+{echo'<script>alert("'.@$_GET['w'].'");</script>';}
+?>
+<script>
+function validateForm() {var y = document.forms["form"]["name"].value;	var letters = /^[A-Za-z]+$/;if (y == null || y == "") {alert("Name must be filled out.");return false;}var z =document.forms["form"]["college"].value;if (z == null || z == "") {alert("college must be filled out.");return false;}var x = document.forms["form"]["email"].value;var atpos = x.indexOf("@");
+var dotpos = x.lastIndexOf(".");if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {alert("Not a valid e-mail address.");return false;}var a = document.forms["form"]["password"].value;if(a == null || a == ""){alert("Password must be filled out");return false;}if(a.length<5 || a.length>25){alert("Passwords must be 5 to 25 characters long.");return false;}
+var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords must match.");return false;}}
+</script>
+<style>
+  		@import url("https://fonts.googleapis.com/css?family=Kalam");
+      .logos{
+        font-family: 'Kalam';
+        color:white;
+        font-size: 30px;
+      }
+      body {
+                width: 100%;
+                background: url('https://t4.ftcdn.net/jpg/03/45/88/07/360_F_345880772_zIT2mkdCzTthplO7xqaGGrMspN0jw0ll.jpg') ;
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: cover;
+            }
+            .center{
+              margin:10% 0% 5% 40%;
+            }
+            .btn{
+              padding:10px 30px;
+
+            }
+            a{
+              text-decoration: none;
+              color: Red;
+              font-size: 20px;
+              font-family: 'Kalam';
+              border-radius: 20px;
+
+            }
+            h1 {
+    color:yellow;
+    text-transform: uppercase;
+    font-size: 100px;
+    font-weight: 700;
+    letter-spacing: 0.015em;
+    font-family: 'Kalam';
+    text-align: center;
+  }
+  h1::after {
+    content: '';
+    width: 500px;
+    display: block;
+    background: red;
+    height: 6px;
+    margin: 0px auto;
+    line-height: 1.1;
+  }
+  .by{
+    margin-right: 200px;
+    font-family: 'Kalam';
+    color:yellow;
+    text-align: right;
+    font-size: 20px;
+    
+
+  }
+</style>
+
+
+</head>
+
+<body>
+<div class="header">
+<div class="row">
+<div class="col-lg-6">
+<span class="logos">Presidio Campus Exam</span></div>
+<div class="col-md-2 col-md-offset-4">
+<a href="#" class="pull-right btn sub1" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Login</b></span></a></div>
+
+
+
+<div class="modal fade" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content title1">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title title1"><span style="color:orange">Log In</span></h4>
+        
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal" action="login.php?q=index.php" method="POST">
+<fieldset>
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-3 control-label" for="email"></label>  
+  <div class="col-md-6">
+  <input id="email" name="email" placeholder="Enter your email-id" class="form-control input-md" type="email">
+    
+  </div>
+</div>
+
+
+<!-- Password input-->
+<div class="form-group">
+  <label class="col-md-3 control-label" for="password"></label>
+  <div class="col-md-6">
+    <input id="password" name="password" placeholder="Enter your Password" class="form-control input-md" type="password">
+    
+  </div>
+</div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Log in</button>
+		</fieldset>
+</form>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!--sign in modal closed-->
+
+</div><!--header row closed-->
+</div>
+
+
+
+</div></div>
+</div><!--container end-->
+
+<br>
+<br>
+<h1>  Presidio Campus Exam</h1>
+<div class="center">
+
+ 
+  <button class="btn"> <a href="register.php">Register</a></button>
+<button class="btn"><a href="#" data-toggle="modal" data-target="#login">Admin Login</a></button>
+</div>
+
+<div class="by">
+  By,<br>
+  <span>Sureka</span>
+</div>
+  
+
+
+
+<!-- Modal For Developers-->
+<div class="modal fade title1" id="developers">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" style="font-family:'typo' "><span style="color:orange">Developers</span></h4>
+      </div>
+	  
+      <div class="modal-body">
+        <p>
+		<div class="row">
+		<div class="col-md-4">
+		 <img src="image/CAM00121.jpg" width=100 height=100 alt="Sunny Prakash Tiwari" class="img-rounded">
+		 </div>
+		 <div class="col-md-5">
+		<a href="https://www.facebook.com/donnieboiii" style="color:#202020; font-family:'typo' ; font-size:18px" title="Find on Facebook">Lyndon Rife Bermoy</a>
+		<h4 style="color:#202020; font-family:'typo' ;font-size:16px" class="title1">+639079373999</h4>
+		<h4 style="font-family:'typo' ">serbermz2020@gmail.com</h4>
+		<h4 style="font-family:'typo' ">Philippine Science High School - Caraga Region Campus</h4></div></div>
+		</p>
+      </div>
+    
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<!--Modal for admin login-->
+	 <div class="modal fade" id="login">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title"><span style="color:orange;font-family:'typo' ">LOGIN</span></h4>
+      </div>
+      <div class="modal-body title1">
+<div class="row">
+<div class="col-md-3"></div>
+<div class="col-md-6">
+<form role="form" method="post" action="admin.php?q=index.php">
+<div class="form-group">
+<input type="text" name="uname" maxlength="20"  placeholder="Admin user id" class="form-control"/> 
+</div>
+<div class="form-group">
+<input type="password" name="password" maxlength="15" placeholder="Password" class="form-control"/>
+</div>
+<div class="form-group" align="center">
+<input type="submit" name="login" value="Login" class="btn btn-primary" />
+</div>
+</form>
+</div><div class="col-md-3"></div></div>
+      </div>
+      <!--<div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>-->
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!--footer end-->
+
+
+</body>
+</html>
